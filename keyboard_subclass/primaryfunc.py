53b2ec14166cb4c548edbc87f42bff016c9ac71d
Nicolas Seemann-Ricard <n.seemannricard@gmail.com>
2020-06-07 00:37:33 +0100

    Added most basic functions and connection to keyboard - Might have to more tone function closer to keyboard for immediate tone

--- keyboard_subclass/primaryfunc.py
+++ keyboard_subclass/primaryfunc.py
@@ -1,6 +1,5 @@
 from keyboardread import inputVal
 from keyboardread import inputTyp
-from keyboardread import inputs
 from keyboardread import dialIn
 
 import threading
@@ -14,23 +13,48 @@ ser = serial.Serial('/dev/ttyAMA0',4800)
 #ser.write(b"ATD"+phoneNum.encode()+b";\r")
 
 handsetSens = DigitalInputDevice(16, pull_up=False)
+phoneNum = ""
 
-def CPTONE(toneVal):
+def CPTONE(toneVal): #might have to move close to keyboard for immediate tone
     ser.write(b"AT+CPTONE="+toneVal.encode()+b";\r")
+    print("tone: " , toneVal)
         
-def VERIFY:
-    
+def VERIFY():
+    #return True
+    #return False
+    if len(phoneNum) < 5:
+        return True
+    else:
+        return "call"
+
+def CALL():
+    ser.write(b"ATD"+phoneNum.encode()+b";\r")
+    print("calling: " , phoneNum)
+    handsetSens.wait_for_active()
+    ser.write(b"AT+CHUP")
+
 
 def main(): 
-    while(1)
-        global phoneNum = ""
+    while(1):
+        global phoneNum
         while (handsetSens.value==0):
             dialVals = dialIn()
             if(dialVals[1] == "num"):
-                CPSTONE(dialVals[0])
+                CPTONE(dialVals[0])
                 phoneNum = phoneNum + dialVals[0]
-                VERIFY
+                print("phone: ", phoneNum)
+                verifOut = VERIFY()
+                if(verifOut == False):
+                   pass 
+                elif(verifOut == True):
+                    pass
+                elif(verifOut == "call"):
+                    CALL()
             elif(dialVals[1] == "spec"):
+                pass
             elif(dialVals[1] == "com"):
+                pass
             elif(dialVals[1] == None):
+                pass
+        phoneNum = ""
 
