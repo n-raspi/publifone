eb3a8ad0fa49bd35f72995dfdc487c6e8ffca82d
Nicolas Seemann-Ricard <n.seemannricard@gmail.com>
2020-06-15 18:36:05 +0100

    Comms handling with FONA on return_test.py Started documenting relevant AT commands and other info

--- return_test.py
+++ return_test.py
@@ -3,14 +3,25 @@ from serial import *
 import time
 from time import *
 val = 0
-ser = serial.Serial('/dev/ttyAMA0',4800)
+ser = serial.Serial('/dev/ttyAMA0',4800, timeout = 1)
+#ser.write(b"ATE0\r")
+#ser.flush()
+ser.reset_input_buffer()
 buffer = ""
-def scr(inputStr,time_out):
+def scr(inputStr):
     buffer = ""
-    ser.write(inputStr.encode() + b"\n\r")
-    ser.flush()
-    
-    val = ser.read_until(b"\r",size = None).decode() 
+    ser.write(inputStr.encode() + b"\r")
+    ser.flush() 
+    val = ser.read_until(b"\r\n")
     print(val)
-
-scr("aaaaaaaaaaaaa")
+    val = ser.read_until(b"\r\n")[:-2].decode()
+    if not val:
+        print("aaaaaaah")
+    else:
+        print(val)
+def nothing(inputStr):
+    ser.write(inputStr.encode() + b"\r")
+    ser.flush()
+    sleep(0.5)
+    ser.reset_input_buffer()
+scr("AT")
