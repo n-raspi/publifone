743be8ac7b232463dc2d8c016908364b6714d404
Nicolas Seemann-Ricard <n.seemannricard@gmail.com>
2020-06-17 19:38:38 +0100

    send check reply check reply

--- return_test.py
+++ return_test.py
@@ -8,35 +8,41 @@ ser = serial.Serial('/dev/ttyAMA0',4800, timeout = serTO)
 #ser.write(b"ATE0\r")
 #ser.flush()
 ser.reset_input_buffer()
-buffer = ""
-def scr(inputStr,timeout):
-    buffer = ""
+buffer = []
+def scir(inputStr,timeout): #send check immediate reply
+    if int(ser.inWaiting()) != 0:
+        print(ser.read(255).decode())
+        ser.reset_input_buffer() 
+        
     ser.write(inputStr.encode() + b"\r")
     ser.flush()
-    if(timeout):
-        for j in range(int(timeout/serTO)):
-            ser.read_until(b"\r\n")#ignore first \r\n
-            val = ser.read_until(b"\r\n")[:-2].decode()
-            if not val:
-                print("aaaaaaah")
-            else:
-                print(val)
-    elif(timeout == None):
-        while(1):
-            ser.read_until(b"\r\n")#ignore first \r\n
-            val = ser.read_until(b"\r\n")[:-2].decode()
-            if not val:
-                print("aaaaaaah")
-            elif val == "OK":
-                print(val)
-                break
-            else:
-                print(val)
-                break;
+    return cr(timeout)
+    
+def cr(timeout): #check reply 
+    buffer = []
+    if(timeout == None): timeout = 40
+    for j in range(int(timeout/serTO)):
+        ser.read_until(b"\r\n")#ignore first \r\n
+        val = ser.read_until(b"\r\n")[:-2].decode()
+        if not val:
+            print("None")
+        if val == "OK" or val == "ERROR":
+            buffer.append(val)
+            print(val)
+            return buffer
+        else:
+            buffer.append(val)
+            print(val)
+            j = 0
     
 def nothing(inputStr):
     ser.write(inputStr.encode() + b"\r")
     ser.flush()
     sleep(0.5)
     ser.reset_input_buffer()
-scr("AT+CBC",2)
+if __name__ == "__main__":
+    print(scir("ATD+447447571213;",10))
+    sleep(1)
+    print(scir("AT+CLCC",3))
+    sleep(1.5)
+    print(scir("AT+CHUP",5))
