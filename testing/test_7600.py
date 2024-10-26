ba2b0c1879e7bf18b59b917fcf1c248e471abdd9
n-raspi <n.seemannricard@gmail.com>
2024-10-26 18:52:29 +0200

    push test

--- testing/test_7600.py
+++ testing/test_7600.py
@@ -1,35 +1,43 @@
 import serial
 import time
 import sys
+# ~ import hashlib
 
 PORT= '/dev/ttyUSB0'
 
-baud = 115200
+baud = 300 # 115200
 
-ser = serial.Serial(PORT,baud,timeout = 1)
-
-
+ser = serial.Serial(PORT,baud,timeout = 5)
 
 
 
-ser.write(str.encode('AT+CPTONE=4\r\n'))
-time.sleep(0.6)
+ser.write(str.encode('AT+\r\n'))
+time.sleep(5)
+inp = ser.read(ser.in_waiting)
+# ~ print(inp)
+
+time.sleep(5)
 inp = ser.read(ser.in_waiting)
 print(inp)
 
-ser.write(str.encode('AT+CRXVOL=0xffff\r\n'))
-time.sleep(0.6)
-inp = ser.read(ser.in_waiting)
-print(inp)
+# ~ ser.write(str.encode('AT+CPTONE=4\r\n'))
+# ~ time.sleep(0.6)
+# ~ inp = ser.read(ser.in_waiting)
+# ~ print(inp)
 
-ser.write(str.encode('AT+CPTONE=4\r\n'))
-time.sleep(0.6)
-inp = ser.read(ser.in_waiting)
-print(inp)
+# ser.write(str.encode('AT+CRXVOL=0xffff\r\n'))
+# time.sleep(0.6)
+# inp = ser.read(ser.in_waiting)
+# print(inp)
 
-ser.write(str.encode('AT+CRXVOL=0x0004\r\n'))
-time.sleep(0.6)
-inp = ser.read(ser.in_waiting)
-print(inp)
+# ser.write(str.encode('AT+CPTONE=4\r\n'))
+# time.sleep(0.6)
+# inp = ser.read(ser.in_waiting)
+# print(inp)
+
+# ser.write(str.encode('AT+CRXVOL=0x0004\r\n'))
+# time.sleep(0.6)
+# inp = ser.read(ser.in_waiting)
+# print(inp)
 
 ser.close()
