699ef8d81698f583f368308951ca4d4f9d17d29d
n-raspi <n.seemannricard@gmail.com>
2024-11-15 09:33:56 +0100

    update_test_7670

--- test_7670_MW.py
+++ test_7670_MW.py
@@ -33,7 +33,9 @@ HTTPACTION = "AT+HTTPACTION=0"
 HTTPHEAD = "AT+HTTPHEAD"
 
 HTTPTERM = "AT+HTTPTERM"
-
+# Connection: keep-alive
+# Server: gunicorn/19.9.0
+# Access-Control-Allow-Origin: *
 # "200 OK" in str
 # AT+HTTPHEAD
 # +HTTPHEAD: 226
@@ -41,18 +43,15 @@ HTTPTERM = "AT+HTTPTERM"
 # Date: Tue, 12 Nov 2024 10:24:17 GMT
 # Content-Type: application/json
 # Content-Length: 254
-# Connection: keep-alive
-# Server: gunicorn/19.9.0
-# Access-Control-Allow-Origin: *
 # Access-Control-Allow-Credentials: true
 
 def logprint(msg, mode=0):
     if mode == 0:
         print(msg)
         logger.info(msg)
-    elif mode = 1:
+    elif mode == 1:
         print(msg)
-    else mode = 2:
+    elif mode == 2:
         logger.info(msg)
 
 
@@ -94,7 +93,7 @@ def read_lines():
         logprint('None read_line (no response)')
         return None
 
-def sent_at(cmd, back, timeout=2):
+def send_at(cmd, back, timeout=2):
     logprint(str.encode(cmd + "\r\n"))
     ser.write(str.encode(cmd + "\r\n"))
     
@@ -103,7 +102,7 @@ def sent_at(cmd, back, timeout=2):
     resp = read_lines()
     if resp != None:
         for i in resp:
-            if back in i:
+            if back in i.decode():
                 return "back in response"
             else:
                 return "back not in response"
@@ -142,10 +141,11 @@ def setup():
     time.sleep(1)
 
     
-    send_at("AT+CBSMOD?", "OK")
+    send_at("AT+CNSMOD?", "OK")
     
     time.sleep(1)
 
+    send_at("AT+HTTPINIT", "OK")
 
 while True:
     
@@ -160,7 +160,7 @@ while True:
     time.sleep(1)
 
     
-    send_at("AT+CBSMOD?", "OK")
+    send_at("AT+CNSMOD?", "OK")
     
     time.sleep(1)
     
@@ -171,9 +171,9 @@ while True:
     sensor = str(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))
     request_command = HTTPPARA + link_suff + f'sensor={sensor}&input={input_val}\"'
     
-    send_at(request_command, 'OK')
+    send_at(request_command, 'OK', timeout=5)
     
-    send_at(HTTPACTION, 'OK')
+    send_at(HTTPACTION, 'OK', timeout=15)
     
-    send_at(HTTPHEAD, 'OK')
+    send_at(HTTPHEAD, 'OK', timeout=5)
     
